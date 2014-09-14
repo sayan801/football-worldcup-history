fe19051494f8772546f855cb56aa7432bd38ade4
Tathagata Banerjee <tatha04banerjee@gmail.com>
2014-09-14 15:52:21 +0530

    js code

---
+++ webcode/worldcup2.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+$(document).ready(function() {
+  $('#clientsOpen').click(function() {
+     $('#clientsDropDown #clientsDashboard').slideToggle({ direction: "up" }, 300);
+ $(this).toggleClass('clientsClose'); 
+  }); // end click
+});
+
+$(document).ready(function(){ 
+    $("#myTab a").click(function(e){
+    	e.preventDefault();
+    	$(this).tab('show');
+    });
+});
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
+});
+
+
+
+
+ 
+
+function GetScorer() {
+    var year = document.getElementById("drpDwnYear").value;
+
+    
+    var scorerName = year.split(" ");
+    document.getElementById("scorerName").innerHTML = "Best Scorer of this World cup was   " + scorerName[1];
+
+    var imageName = scorerName[1] + ".jpg";
+
+    var html = '<img border="0" src="images/'+imageName+'" alt="Pulpit rock" width="304" height="228">';
+
+    $("#scorerName").append(html);
+    }
+    
